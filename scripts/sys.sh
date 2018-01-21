3e99cc7edc42c6b7c9e753510b212b659a835d17
Bread <32032674+buyBread@users.noreply.github.com>
2018-01-21 19:50:37 +0200

    sys.sh, a really bad script i made for myself

---
+++ scripts/sys.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# - colors -
+BLACK='\033[0;30m'
+PINK='\033[1;31m'
+NC='\033[0m' # No Color
+
+# - output -
+echo ""
+echo -e "	 ${PINK}os${BLACK}:${NC} ubuntu"
+echo -e "${BLACK} ┌────┐  ${PINK}kernel${BLACK}:${NC} 4.13.0-25-generic"
+echo -e "${BLACK} │ ${PINK}~*${BLACK} │  ${PINK}shell${BLACK}:${NC} zsh"
+echo -e "${BLACK} └────┘  ${PINK}cpu${BLACK}:${NC} i7-3632qm"
+echo -e "	 ${PINK}init${BLACK}:${NC} systemd"
+echo ""
