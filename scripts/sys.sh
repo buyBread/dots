cb8a04c195f1287eb4a80c581d8b204636faa1f0
Bread <32032674+buyBread@users.noreply.github.com>
2018-02-01 18:58:13 +0200

    small update

--- scripts/sys.sh
+++ scripts/sys.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # - colors -
-BLACK='\033[0;30m'
-PINK='\033[1;31m'
+W='\033[0;37m' # Pink
+P='\033[0;36m' # White
 NC='\033[0m' # No Color
 
 # - output -
 echo ""
-echo -e "	 ${PINK}os${BLACK}:${NC} ubuntu"
-echo -e "${BLACK} ┌────┐  ${PINK}kernel${BLACK}:${NC} 4.13.0-25-generic"
-echo -e "${BLACK} │ ${PINK}~*${BLACK} │  ${PINK}shell${BLACK}:${NC} zsh"
-echo -e "${BLACK} └────┘  ${PINK}cpu${BLACK}:${NC} i7-3632qm"
-echo -e "	 ${PINK}init${BLACK}:${NC} systemd"
+echo -e "	${P}os${W}:${NC} arch"
+echo -e "${W} ┌───┐  ${P}wm${W}:${NC} openbox"
+echo -e "${W} │ ${P}+${W} │  ${P}shell${BLACK}:${NC} zsh"
+echo -e "${W} └───┘  ${P}cpu${W}:${NC} i7-3632qm"
+echo -e "	${P}init${W}:${NC} systemd"
 echo ""
