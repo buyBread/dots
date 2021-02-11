21be5160f3478415cf07ff11176287a171979936
buyBread <buymybreadalt@gmail.com>
2021-02-11 18:20:27 +0200

    smooth

--- .scripts/song.py
+++ .scripts/song.py
@@ -11,13 +11,13 @@ try:
         "org.freedesktop.DBus.Properties"
     )
 except dbus.exceptions.DBusException:
-    exit("Nothing is playing")
+    exit()
 
 metadata = spotify_bus_properties.Get("org.mpris.MediaPlayer2.Player", "Metadata")
 playback_status = spotify_bus_properties.Get("org.mpris.MediaPlayer2.Player", "PlaybackStatus")
 
 if metadata['xesam:artist'][0] == "":
-    exit("Advertisement")
+    exit()
 
 # the fuck is xesam bitch
 # why the fuck is artist an array??
